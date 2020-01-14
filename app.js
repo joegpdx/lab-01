1db2ba592e8ce2f462ae601ef3a470492679725b
joegpdx <joegpdx@gmail.com>
2020-01-14 09:24:00 -0800

    edits

--- app.js
+++ app.js
@@ -1,6 +1,19 @@
-console.log(Hello)
+import isYes from './isYes.js';
 
-const quizButton = document.getElementById("quizzbutton");
+console.log("Hello")
 
-const total = document.getElementById("Score");
+const quizButton = document.getElementById('quizbutton');
+const total = document.getElementById('Score');
 
+quizbutton.addEventListener('click', () => {
+    console.log('buttonworks');
+    const name = prompt('What\'s your name?');
+    const question1 = prompt('Do I like key-lime Pie?');
+    const question2 = prompt('Do I love Stere\'s');
+    const question3 = prompt('Do I love Polenta?');
+    let answers = 0;
+    if (!isYes(question1)) { count++ }
+    if (isYes(question2)) { count++ }
+    if (isYes(question3)) { count++ }
+    total.textContent = '${name}, you got ${count}, correct';
+})
---
+++ isYes.js
@@ -0,0 +1,25 @@
+export default function isYes(str) {
+    // if (str === 'Yes') {
+    //     return true;
+    // } else {
+    //     return false;
+    // }
+    if (str === 'Y') {
+        return true;
+    } else if (str === 'yes') {
+        return true;
+    } else if (str === 'y') {
+        return true;
+    } else if (str === 'Yes') {
+        return true;
+    } else return false;
+}
+
+
+
+
+// if (kushdklfgh) {
+
+// } else if(str === 'yep') {
+
+// }
