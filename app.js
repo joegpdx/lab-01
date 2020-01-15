58a51829bac576930e2e34d00fa8c6c3ea758223
joegpdx <joegpdx@gmail.com>
2020-01-15 07:16:54 -0800

    edits-setting-up-lab

--- app.js
+++
@@ -1,8 +0,0 @@
-import compare from './compare.js';
-
-const tries = document.getElementById('tries');
-const guessnumber = getElementById('guess-number');
-const highlow = getElementById('high-low');
-const losewin = getElementById('lose-win');
-
-let 
---
+++ compare.js
@@ -0,0 +1,10 @@
+export default function compareNumbers(userGuess, correctNumber) {
+    if (!Number.isInteger(userGuess)) throw new Error('Please Enter a Number!');
+    if (userGuess === correctNumber) {
+        return 0;
+    } else if (userGuess < correctNumber) {
+        return -1;
+    } else if (userGuess > correctNumber) {
+        return 1;
+    }
+}
