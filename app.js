0fff9996586c1f525fa569e2c35435326b5b0961
joegpdx <joegpdx@gmail.com>
2020-01-14 18:01:21 -0800

    start-app.js

---
+++ app.js
@@ -0,0 +1,8 @@
+import compare from './compare.js';
+
+const tries = document.getElementById('tries');
+const guessnumber = getElementById('guess-number');
+const highlow = getElementById('high-low');
+const losewin = getElementById('lose-win');
+
+let 
